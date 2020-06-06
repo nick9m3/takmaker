593bedce1030977173b3fc849b3e522125fed21c
nick9m3 <64651449+nick9m3@users.noreply.github.com>
2020-06-06 15:09:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ let check = document.querySelector('.check-time');
 // variable of tag form to input date
 const formDa = document.querySelector('.formdate');
 
+const dele = document.querySelector('.delete');
+
 let taskdate =false;
 
 // function to read the value of input
@@ -20,28 +22,41 @@ function readtask(){
     inputtas.addEventListener('submit', addtask);
     check.addEventListener('change', checkTime);
     remove.addEventListener('click', removeAll);
+    list.addEventListener('click', removet);
 }
 
 //fuction will remove all the task
 
 function removeAll(){
-    remover('.list-item');
+        while(list.firstChild){
+            list.removeChild(list.firstChild);
+        }
+    
 }
     
 
 // function that make the li item with the value of input (.text class or taskin variable)
 function addtask(e){
     if(taskin.value === '') {
-        alert('scrivi');
+        alert('Ah scriva nguna cosa');
     }
     else{
         const li = document.createElement('li');
+        const del = document.createElement('a');
         li.className = 'list-item';
+        del.className ='delete';
+        del.appendChild(document.createTextNode("⊠"));
         if(taskdate){
             var date = document.querySelector('.setDate');
-            li.appendChild(document.createTextNode(taskin.value + ' ' + date.value));
+            const second = document.createElement('a');
+            second.className ='hour';
+            second.appendChild(document.createTextNode(date.value));
+            li.appendChild(del);
+            li.appendChild(document.createTextNode(taskin.value));
+            li.appendChild(second);
             }
         else{
+            li.appendChild(del);
             li.appendChild(document.createTextNode(taskin.value));
             
         }
@@ -55,7 +70,7 @@ function addtask(e){
 function checkTime(e){
     if(e.target.checked){
         const date =document.createElement('input');
-        date.setAttribute('type', 'date');
+        date.setAttribute('type', 'time');
         date.className='setDate';
         formDa.appendChild(date);
         taskdate = true;
@@ -67,4 +82,10 @@ function checkTime(e){
 }
 function remover(x){
     var rem= document.querySelector(x).remove();
+}
+
+function removet(x){
+    if(x.target.classList.contains('delete')){
+        x.target.parentElement.remove();
+    }
 }
